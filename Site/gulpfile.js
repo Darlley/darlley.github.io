43905de4dea442ef7d66ea0bb03796aab72cc81e
Darlley <systembrtt@gmail.com>
2021-06-03 22:29:12 -0700

    menu

--- Site/gulpfile.js
+++ Site/gulpfile.js
@@ -1,31 +1,33 @@
-// GULP (compilação)
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('sass', gulp.series( function() {
-    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
-    .pipe(sass({outputStyle: 'compressed'})) // converter o Sass em CSS
-    .pipe(gulp.dest('src/css'));
-}));
-
-gulp.task('watch', gulp.series( function() {
-    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.parallel( ['sass'] ));
-}));
-
-gulp.task('default', gulp.series( ['sass', 'watch'] ));
-
-// GULP SERVE (SERVIDOR WEB)
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var sass        = require('gulp-sass');
 
-gulp.task('serve', function () {
-    browserSync.init({
-        server: {
-            baseDir: "src"
-        }
-    });
-
-    gulp.watch("*.html").on("change", reload);
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series('sass', function() {
+    browserSync.init({
+        server: "./src"  
+    });
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.parallel('js','serve'));
+
+// CRÉDITOS:
+// 1. Ricardo Sanches - https://www.youtube.com/watch?v=5BED0R0G3ow
+// 2. Atualização - https://gist.github.com/ntry/8a87ecb8427f044211dc3bfa390ff971
