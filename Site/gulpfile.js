cec7f51437afc6796f91ed31aa042e5b815ed67a
Darlley <37590954+Darlley@users.noreply.github.com>
2021-06-06 10:15:44 -0700

    minify css

--- Site/gulpfile.js
+++ Site/gulpfile.js
@@ -5,7 +5,7 @@ var sass        = require('gulp-sass');
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
